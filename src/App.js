1fb1d65ad5a9d3eb6266e63ceff561b96298698f
Outili Rayan <rayan.outili@etu.unice.fr>
2023-06-03 23:23:47 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        backgroundColor: 'black',
+        color: 'white',
+        fontSize: '24px',
+      }}
+    >
+      Soon...
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+export default App;
