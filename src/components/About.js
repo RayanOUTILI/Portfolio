0b4c38b5ca5522694a9b5d62c3d82c00f4add298
Outili Rayan <rayan.outili@etu.unice.fr>
2023-06-18 14:05:23 +0200

    night mode

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <>
         <section id="about">
-            <div className="text-center">
+            <div className="text-center pt-20">
                 <h1 className="">Soon...</h1>
             </div>
         </section>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,8 +6,8 @@ import { Link } from 'react-router-dom';
 
 const Menu = ({ toggleDarkMode }) => {
 	return (
-		<nav className='bg-[var(--background-color)] flex z-50 items-center justify-between fixed top-0 left-0 w-full h-20 p-7 pl-10 pr-20 shadow-md'>
-			<h1 className="mr-auto taille font-bold text-gray-800">Rayan Outili</h1>
+		<nav id="menu" className='bg-[var(--background-menu-color)] flex z-50 items-center justify-between fixed top-0 left-0 w-full h-20 p-7 pl-10 pr-20 shadow-md'>
+			<h1 className="mr-auto taille font-bold ">Rayan Outili</h1>
 			<ul className="flex list-none space-x-4">
 				<li>
 					<Link to="" onClick={(e) => e.preventDefault()}>Home</Link>
