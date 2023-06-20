4898f2dc68768ccbf5414ad4d5523cbd577316fb
Outili Rayan <rayan.outili@etu.unice.fr>
2023-06-20 20:59:53 +0200

    responsive stacks

--- src/components/About.js
+++ src/components/About.js
@@ -5,13 +5,20 @@ import CSS3 from "../assets/icons/css.png";
 import JS from "../assets/icons/js.png";
 import REACT from "../assets/icons/react.png";
 import TAILWIND from "../assets/icons/tailwindcss.png";
+import Java from "../assets/icons/java.png";
+import Python from "../assets/icons/python.png";
+import C from "../assets/icons/c.png";
+import Php from "../assets/icons/php.png";
+import Sql from "../assets/icons/sql.png";
+
 
 const About = () => {
     return (
         <>
             <section id="about">
-            <h1 className="text-4xl font-bold text-left ml-10 mt-10 mb-2">About Me</h1>
-            <h2 className="text-2xl font-bold text-center mb-2">Front-end</h2>
+                <h1 className="text-4xl font-bold text-left ml-10 mt-10 mb-2">About Me</h1>
+                
+                <h2 className="text-2xl font-bold text-center mb-2 ">Front-end</h2>
                 <div className="stacks flex justify-center items-center w-[50%] m-auto">
                     <ul className="flex gap-6">
                         <li data-language="HTML5">
@@ -28,6 +35,27 @@ const About = () => {
                         </li>
                         <li data-language="TailwindCSS">
                             <img src={TAILWIND} alt="TailwindCSS"></img>
+                        </li>
+                    </ul>
+                </div>
+
+                <h2 className="text-2xl font-bold text-center mb-2">Back-end</h2>
+                <div className="stacks flex justify-center items-center w-[50%] m-auto">
+                    <ul className="flex gap-6">
+                        <li data-language="Java">
+                            <img src={Java} alt="Java"></img>
+                        </li>
+                        <li data-language="Python">
+                            <img src={Python} alt="Python"></img>
+                        </li>
+                        <li data-language="C">
+                            <img src={C} alt="C"></img>
+                        </li>
+                        <li data-language="Php">
+                            <img src={Php} alt="Php"></img>
+                        </li>
+                        <li data-language="Sql">
+                            <img src={Sql} alt="Sql"></img>
                         </li>
                     </ul>
                 </div>
