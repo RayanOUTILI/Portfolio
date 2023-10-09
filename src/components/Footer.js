7b3ac1efc61a49d93b879b2e9fb10b5310127d61
Rayan Outili <rayan.outili@etu.unice.fr>
2023-10-09 22:15:03 +0200

    footer prov + up proj

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,13 @@ const currentYear = new Date().getFullYear();
 
 const Footer = () => {
     return (
-        <section id="footer">
-            <div className="container mt-12 bg-black text-white  h-[200px] w-[100%] justify-center flex items-center">
-            <h2>↑Rayan Outili </h2>
-            <p> ©{currentYear}</p>
-
+        <section className="container mt-12 bg-black text-white h-[200px] w-[100%] justify-center flex items-center" id="footer">
+            <div className="justify-center flex items-center">
+                <h2><a href="#home">↑Rayan Outili</a> </h2>
+                <p> ©{currentYear} - </p>
+            </div>
+            <div>
+                <p className="block"> Contact : <a href="mailto:rayan.outili@gmail.com">rayan.outili@gmail.com</a></p>
             </div>
         </section>
     )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,6 +10,7 @@ import c from "../assets/icons/c.png";
 import python from "../assets/icons/python.png";
 import react from "../assets/icons/react.png";
 import tailwind from "../assets/icons/tailwindcss.png";
+import geometrydash from "../assets/img/geometrydash.png";
 
 const projectsData = [
     {
@@ -22,21 +23,21 @@ const projectsData = [
     },
     {
         title: "Convertisseur d'images",
-        description: "Page web permettant à l'utilisateur de sélectionner une image et lui appliquer un traitement",
+        description: "Page web permettant à l'utilisateur de sélectionner une image et lui appliquer un traitement.",
         url: "https://rayanoutili.github.io/convertisseurImages/",
         githubUrl: "https://github.com/RayanOUTILI/convertisseurImages",
         techno: [html, css, c, python]
     },
     {
         title: "Enquête écologique",
-        description: "Site web proposant une enquête aux entreprises de Nice sur leurs pratiques écologiques - réalisé en collaboration",
+        description: "Site web proposant une enquête aux entreprises de Nice sur leurs pratiques écologiques - réalisé en collaboration.",
         url: "https://rayanoutili.github.io/site-ecologie/",
         githubUrl: "https://github.com/RayanOUTILI/site-ecologie",
         techno: [html, css]
     },
     {
         title: "Moviedle",
-        description: "Page web permettant de deviner des films à partir d'une affiche floutée - réalisé en collaboration",
+        description: "Page web permettant de deviner des films à partir d'une affiche floutée - réalisé en collaboration.",
         url: "https://diosx17.github.io/MoviedleREWORK/",
         githubUrl: "https://github.com/Diosx17/MoviedleREWORK",
         techno: [html, css, js]
@@ -44,11 +45,21 @@ const projectsData = [
     {
         title: "Portfolio",
         description: "Mon Portfolio développé en ReactJS.",
-        url: "rayan-outili.fr",
+        url: "https://rayan-outili.fr",
         githubUrl: "https://github.com/RayanOUTILI/Portfolio",
         techno: [html, css, js, react, tailwind]
     }
 ];
+
+const PythonProjects = [
+    {
+        title: "Geometry dash",
+        description: "Programme utilisant le module Turtle de Python pour réaliser un visuel du jeu Geometry Dash.",
+        url: [geometrydash],
+        githubUrl: "https://github.com/Diosx17/MoviedleREWORK",
+        techno: [python]
+    }
+]
 
 const Projects = () => {
     return (
@@ -63,6 +74,41 @@ const Projects = () => {
                             <div className="gauche w-65% relative">
                                 <a href={project.url} target="_blank" rel="noopener noreferrer">
                                     <iframe id="iframe" title={project.title} className="iframe w-[40vw] h-[40vh] m-[3%]" src={project.url} alt={project.title} />
+                                </a>
+                            </div>
+
+                            {/* Partie droite (titre, descri, techno, liens) */}
+                            <div className="droite w-40%">
+                                <h2 className="text-2xl font-bold">{project.title}</h2>
+                                <p className="text-gray-600 mt-2 justify-center flex items-center">{project.description}</p>
+                                <div className="techno flex justify-start gap-4 mt-4">
+                                    {project.techno.map((techno, index) => (
+                                        <img className="h-[38px] w-[38px]" src={techno} alt="techno" key={index} />
+                                    ))}
+                                </div>
+                                <div className="mt-6 flex items-center justify-start gap-4">
+                                    <a href={project.url} target="_blank" rel="noopener noreferrer" className="link  flex items-center">
+                                        <img src={link} alt="link-icon" className="w-4 h-4 mr-1" />
+                                        Site
+                                    </a>
+                                    <a href={project.githubUrl} target="_blank" rel="noopener noreferrer" className="link flex items-center">
+                                        <img src={code} alt="code-icon" className="w-4 h-4 mr-1" />
+                                        Code
+                                    </a>
+                                </div>
+
+                            </div>
+                        </div>
+                    ))}
+                    {/* python project img /- iframe */}
+                    {PythonProjects.map((project, index) => (
+                        //revealxINDEX pour alterner les anim revealx${index+1}
+                        <div key={index} className={`reveal proj flex items-center border rounded-3xl gap-[5%] w-[60%] mt-[2%] m-auto bg-white`}>
+                            {/* Partie gauche (iframe) */}
+                            <div className="gauche w-65% relative">
+                                <a href={project.url} target="_blank" rel="noopener noreferrer">
+                                    <img src={project.url} id="iframe" title={project.title} className="iframe w-[100vw] h-[40vh] m-[3%]" alt={project.title} />
+                                    {console.log(project.url)}
                                 </a>
                             </div>
 
