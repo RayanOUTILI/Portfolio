315b1c73864a9ea0c172b20210b98d06063a5c28
Rayan Outili <rayan.outili@etu.unice.fr>
2024-02-25 21:21:46 +0100

    skills style + contact soon

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <section className='mt-[40vh]'>
+            <h1 className="text-3xl font-bold text-center mb-8">A venir...</h1>
+            {/* <a href="mailto:rayan.outili@gmail.com" className="text-2xl font-bold text-center">Me contacter</a> */}
+        </section>
+    );
+};
+
+export default Contact;
